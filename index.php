4bedf5545ea822713d1b8b037fc20cfa16c608a0
FY Kuo <teresa99339@gmail.com>
2022-09-09 15:32:31 +0800

    整理檔案 完成base檔案 完成累積人數功能

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-﻿<!DOCTYPE html
+﻿<?php
+$do = ($_GET['do'])??'main';
+include('./api/base.php');
+?>
+<!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -19,14 +23,19 @@
     </div>
 
     <div id="all">
+
         <div id="title">
-            <?=date('m')?> 月  <?=date('d')?> 號  <?=date('l')?> | 今日瀏覽: 1 | 累積瀏覽: 36 </div>
+            <?=date('m')?> 月  <?=date('d')?> 號  <?=date('l')?> | 今日瀏覽: <?=$View->find(['date'=>$today])['total']?> | 累積瀏覽: <?=$viewSum?> 
+		</div>
+
         <div id="title2" title="回首頁">
 			<a href="./index.php">
 				<img src="./icon/02B01.jpg" alt="回首頁">
 			</a>
         </div>
+
         <div id="mm">
+
             <div class="hal" id="lef">
                 <a class="blo" href="?do=po">分類網誌</a>
                 <a class="blo" href="?do=news">最新文章</a>
@@ -34,21 +43,34 @@
                 <a class="blo" href="?do=know">講座訊息</a>
                 <a class="blo" href="?do=que">問卷調查</a>
             </div>
+
             <div class="hal" id="main">
                 <div>
-
+                    <span style="width:78%; display:inline-block;">
+						<marquee>請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地！詳見最新文章</marquee>
+                    </span>
                     <span style="width:18%; display:inline-block;">
                         <a href="?do=login">會員登入</a>
                     </span>
-                    <div class="">
+
+                    <div class="content">
+						<?php
+						if(file_exists('./front/'.$do.'.php')){
+							include('./front/'.$do.'.php');
+						}else{
+							include('./front/main.php');
+						}
+						?>
                     </div>
                 </div>
             </div>
+
         </div>
+
         <div id="bottom">
-            本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved
+            本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2022健康促進網社群平台 All Right Reserved
             <br>
-            服務信箱：health@test.labor.gov.tw<img src="./home_files/02B02.jpg" width="45">
+            服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
         </div>
     </div>
 
